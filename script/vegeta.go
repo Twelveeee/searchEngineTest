c82549fa221dc490385ccee78918c0686cda80d8
Twelveeee <Twelveeee12@gmail.com>
2023-11-25 05:35:55 +0000

    pressure test

--- script/vegeta.go
+++ script/vegeta.go
@@ -13,11 +13,16 @@ func pressureTest(targeter vegeta.Targeter, rate vegeta.ConstantPacer, duration 
 
 	fmt.Printf("start pressure test, duration:%s , rate: %d/s\n\n", duration, rate.Freq)
 	var metrics vegeta.Metrics
+
+	count := 1
 	for res := range attacker.Attack(targeter, rate, duration, "Big Bang!") {
+		fmt.Printf("%d, response:%s \n", count, string(res.Body)[0:100])
+		count++
 		metrics.Add(res)
 	}
 	metrics.Close()
 
+	fmt.Printf("\n\n ======= report ======= \n\n")
 	report := vegeta.NewTextReporter(&metrics)
 	report.Report(os.Stdout)
 
